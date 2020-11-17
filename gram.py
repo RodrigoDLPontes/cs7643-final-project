1c6c49d4bd4f67cbc462ed797069573b18dcb2b1
Rodrigo Deleu Lopes Pontes <rodrigo.dl.pontes@gmail.com>
2020-11-17 13:53:58 -0500

    Added convenience prefix parameter

--- gram.py
+++ gram.py
@@ -8,12 +8,13 @@ from torch.utils.data import Dataset
 class GRAM_RTM(Dataset):
     """GRAM Road-Traffic Monitoring (GRAM-RTM) dataset."""
 
-    def __init__(self, csv_path="num_cars_corrected.csv", img_path="M-30", roi_path="ROI.jpg"):
+    def __init__(self, csv_path="num_cars_corrected.csv", img_path="M-30", roi_path="ROI.jpg", prefix=""):
         """
         Args:
             csv_file (string): Path to the csv file with labels.
             root_dir (string): Directory with all the M-30 frames.
         """
+        csv_path, img_path, roi_path = map(lambda p: os.path.join(prefix, p), (csv_path, img_path, roi_path))
         self.labels = pd.read_csv(csv_path)
         self.img_path = img_path
         roi = io.imread(roi_path)
