c3853145e5a6b660803113ceaf9030e83303884a
Rodrigo Deleu Lopes Pontes <rodrigo.dl.pontes@gmail.com>
2020-11-18 14:58:23 -0500

    Made updates

--- act_maps.py
+++ act_maps.py
@@ -1,16 +1,13 @@
 import os
-import PIL
 import torch
 import pandas as pd
-from skimage import io
-from torchvision import transforms
 from torch.utils.data import Dataset
 
-class GRAM_RTM(Dataset):
+class ActivationMaps(Dataset):
     """GRAM Road-Traffic Monitoring (GRAM-RTM) dataset."""
 
-    def __init__(self, csv_path="num_cars_corrected.csv", img_path="M-30",
-                 roi_path="ROI.jpg", prefix="", split=None, spec_nn=False):
+    def __init__(self, csv_path="num_cars_corrected.csv", faster_path="faster_maps",
+                 mask_path="mask_maps", prefix="", split=None, rcnn="mask"):
         """
         Args:
             csv_path (string): Path to the csv file with labels.
@@ -18,10 +15,12 @@ class GRAM_RTM(Dataset):
             roi_path (string): Path to ROI mask.
             prefix (string): Prefix to be added to all paths.
         """
-        csv_path, img_path, roi_path = map(lambda p: os.path.join(prefix, p), (csv_path, img_path, roi_path))
+        csv_path, faster_path, mask_path = map(lambda p: os.path.join(prefix, p), (csv_path, faster_path, mask_path))
+
         all_labels = pd.read_csv(csv_path)
         num_train = int(len(all_labels) * 0.6)
         num_val_test = int(len(all_labels) * 0.2)
+
         if split == "train":
             self.labels = all_labels[:num_train]
             self.offset = 0
@@ -34,14 +33,11 @@ class GRAM_RTM(Dataset):
         else:
             self.labels = all_labels
             self.offset = 0
-        self.img_path = img_path
-        roi = io.imread(roi_path)[:,:,0] // 255
-        roi = torch.tensor(roi).unsqueeze(0)
-        self.spec_nn = spec_nn
-        if self.spec_nn:
-            self.roi = transforms.Resize((65, 65), PIL.Image.NEAREST)(roi)
+
+        if rcnn == "mask":
+            self.act_map_path = mask_path
         else:
-            self.roi = roi
+            self.act_map_path = faster_path
 
     def __len__(self):
         return len(self.labels)
@@ -51,20 +47,11 @@ class GRAM_RTM(Dataset):
             idx = idx.item()
 
         idx += self.offset
-        file_name = f"image{idx + 1:06}.jpg"
-        file_path = os.path.join(self.img_path, file_name)
-        image = io.imread(file_path)
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-        image = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])(image)
-        if self.spec_nn:
-            image = transforms.Resize((65, 65))(image)
-        masked_img = image * self.roi
+        file_name = f"map{idx + 1:06}.pt"
+        file_path = os.path.join(self.act_map_path, file_name)
+        act_map = torch.load(file_path).squeeze()
 
         num_cars = self.labels.loc[idx][0]
-        sample = {'image': masked_img, 'num_cars': num_cars}
 
+        sample = {'act_map': act_map, 'num_cars': num_cars}
         return sample
